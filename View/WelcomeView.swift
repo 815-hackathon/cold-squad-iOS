59f36914156554caff6e09135aa2e519e2554289
부엉이 <o.o.wl.soft@gmail.com>
2019-08-14 22:25:53 +0900

    feat: Create WelcomeView #1

---
+++ View/WelcomeView.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ColdSquad
+//
+//  Created by 이동영 on 14/08/2019.
+//  Copyright © 2019 부엉이. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeView: UIViewController {
+
+    // MARK: - Properties
+    @IBOutlet weak var noteButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
